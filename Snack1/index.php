809c09e71b08a70fe3104782af4bcd1a4c926895
GiovanniT94 <giovanni.t94@hotmail.it>
2022-12-14 18:33:15 +0100

    Push Snack1 completo

---
+++ Snack1/index.php
@@ -0,0 +1,43 @@
+<?php
+    $matches = [
+        [
+            'squadraCasa' => 'Olimpia Milano',
+            'squadraOspite' => 'Cantù',
+            'punteggio' => '55-60'
+        ],
+        [
+            'squadraCasa' => 'Olimpia Milano',
+            'squadraOspite' => 'Virtus Segafredo Bologna',
+            'punteggio' => '70-100'
+        ],
+        [
+            'squadraCasa' => 'GeVi Napoli Basket',
+            'squadraOspite' => 'Virtus Segafredo Bologna',
+            'punteggio' => '50-70'
+        ],
+        [
+            'squadraCasa' => 'GeVi Napoli Basket',
+            'squadraOspite' => 'Cantù',
+            'punteggio' => '85-30'
+        ]
+    ];
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Snack 1</title>
+</head>
+<body>
+    <ul>
+        <?php for ($i=0; $i < count($matches); $i++) { ?>
+            <li><?php echo $matches[$i]['squadraCasa']; ?> - <?php echo $matches[$i]['squadraOspite']; ?> | <?php echo $matches[$i]['punteggio']; ?></li>
+        <?php } ?>
+    </ul>
+</body>
+</html>
